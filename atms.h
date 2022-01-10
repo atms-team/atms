8f14b650a89b2cbd4ee1c65c92b160c6e03c2175
AB0TJ <alex@ab0.tj>
2022-01-10 15:48:46 -0700

    Classes can't be static... duh.

--- atms.h
+++ atms.h
@@ -3,38 +3,38 @@
 
 namespace ATMS
 {
-    static class PacketType
+    class PacketType
     {
         public:
-            const uint8_t Test = 0;
-            const uint8_t Position = 1;
-            const uint8_t Status = 2;
-            const uint8_t Object = 3;
-            const uint8_t Message = 4;
-            const uint8_t Ack = 5;
-            const uint8_t TelemetryData = 6;
-            const uint8_t TelemetryDefinition = 7;
-            const uint8_t Weather = 8;
-            const uint8_t SubscriptionRequest = 9;
-            const uint8_t LocalInfo = 10;
-            const uint8_t Comment = 11;
-            const uint8_t UserDefined = 127;
-            const uint8_t RoutingAnnouncement = 128;
-            const uint8_t Traceroute = 129;
-            const uint8_t Ack = 255;
+            static const uint8_t Test = 0;
+            static const uint8_t Position = 1;
+            static const uint8_t Status = 2;
+            static const uint8_t Object = 3;
+            static const uint8_t Message = 4;
+            static const uint8_t MessageAck = 5;
+            static const uint8_t TelemetryData = 6;
+            static const uint8_t TelemetryDefinition = 7;
+            static const uint8_t Weather = 8;
+            static const uint8_t SubscriptionRequest = 9;
+            static const uint8_t LocalInfo = 10;
+            static const uint8_t Comment = 11;
+            static const uint8_t UserDefined = 127;
+            static const uint8_t RoutingAnnouncement = 128;
+            static const uint8_t Traceroute = 129;
+            static const uint8_t Ack = 255;
     };
 
-    static class Flags
+    class Flag
     {
         public:
-            const uint8_t LatSouth = 0x10;
-            const uint8_t HasAltitude = 0x20;
-            const uint8_t HasCourse = 0x40;
-            const uint8_t HasSpeed = 0x80;
-            const uint8_t LonWest = 0x10;
-            const uint8_t HasPHGD = 0x20;
-            const uint8_t HasTimestamp = 0x40;
-            const uint8_t MessageSupport = 0x80;
+            static const uint8_t LatSouth = 0x10;
+            static const uint8_t HasAltitude = 0x20;
+            static const uint8_t HasCourse = 0x40;
+            static const uint8_t HasSpeed = 0x80;
+            static const uint8_t LonWest = 0x10;
+            static const uint8_t HasPHGD = 0x20;
+            static const uint8_t HasTimestamp = 0x40;
+            static const uint8_t MessageSupport = 0x80;
     };
 }
 
