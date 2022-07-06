9e8b87840fd841000ced8ef16454da6cbc64f0dd
ab0tj <alex@delorted.com>
2022-07-06 13:52:38 -0600

    Add some flag constants

--- atms.h
+++ atms.h
@@ -23,7 +23,7 @@ namespace ATMS
         Ack = 255
     };
 
-    class Flag
+    class PositionFlag
     {
     public:
         static const uint8_t South = 0x10;
@@ -34,6 +34,19 @@ namespace ATMS
         static const uint8_t HasPHGD = 0x20;
         static const uint8_t HasTimestamp = 0x40;
         static const uint8_t MessageSupport = 0x80;
+    };
+
+    class NodeFlag
+    {
+        static const uint8_t hasAtmsHam = 0x01;
+        static const uint8_t hasAtmsISM = 0x02;
+        static const uint8_t hasAtmsCore = 0x04;
+        static const uint8_t canGateToAprsIS = 0x08;
+        static const uint8_t canGateToAprsRF = 0x10;
+        static const uint8_t timeSourceNone = 0;
+        static const uint8_t timeSourceLocalUnv = 1;
+        static const uint8_t timeSourceLocalVer = 2;
+        static const uint8_t timeSourceSynced = 3;
     };
 }
 
